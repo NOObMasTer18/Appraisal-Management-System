10be62bab4f5e5349527d99be0fba9b87afe17e1
Akshay Singh <akshaysingh48641@gmail.com>
2026-04-28 11:06:46 +0530

    feat: update CORS configuration to allow additional origins

--- src/main/java/com/psi/appraisal/config/Config.java
+++ src/main/java/com/psi/appraisal/config/Config.java
@@ -27,7 +27,7 @@ public class Config {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://gumdrop-showing-malt.ngrok-free.dev", "https://appraisal-management-system-fronten.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
